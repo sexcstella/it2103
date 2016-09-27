57c457559bff3f9935a729614f597eaf15917bd8
sexcstella <sexcstella@gmail.com>
2016-09-27 14:15:22 +0800

    added glyphicon user

--- home.php
+++ home.php
@@ -4,6 +4,8 @@
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
-<button class='btn btn-danger'>red button</button>
+<button class='btn btn-danger'>
+	<span class='glyphicon glyphicon-user'></span>
+</button>
 </body>
 </html>
