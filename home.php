182d17afc3140bbf5a6bc9faa0a5f2d30618b015
sexcstella <sexcstella@gmail.com>
2016-09-27 13:46:20 +0800

    added home.php

---
+++ home.php
@@ -0,0 +1,9 @@
+
+<html>
+<head>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -3,4 +3,6 @@
 	for($x = 10; $x > 0; $x--){
 		echo "This is a linya!"
 	}
+
+	include("home.php");
 ?>.
