435b0305eb95702a0f2385c4f638d58d2ac89635
kakakirbz <thefourth006@gmail.com>
2016-09-27 14:23:31 +0800

    added glyphicon remove

--- home.php
+++ home.php
@@ -4,6 +4,7 @@
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
-<button class='btn btn-danger'>red button</button>
+<button class='btn btn-danger'>
+	<i class='glyphicon glyphicon-remove'></i></button>
 </body>
 </html>
